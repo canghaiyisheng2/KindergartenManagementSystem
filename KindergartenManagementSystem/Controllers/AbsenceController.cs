16fec327146a1dcb15477a8ce1c8fccd63b94f9f
shui-dun <2604697844@qq.com>
2021-09-07 11:27:00 +0800

    初始化请假管理模块，并设计了其大部分view

---
+++ KindergartenManagementSystem/Controllers/AbsenceController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KindergartenManagementSystem.Controllers
+{
+    public class AbsenceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult studentAskForAbsence()
+        {
+            return View();
+        }
+
+        public IActionResult studentListAbsence()
+        {
+            return View();
+        }
+
+        public IActionResult studentDisplayAbsenceInfo()
+        {
+            return View();
+        }
+
+        public IActionResult studentReportedBackFromLeave()
+        {
+            return View();
+        }
+
+        public IActionResult teacherListAbsence()
+        {
+            return View();
+        }
+
+        public IActionResult teacherDisplayAbsenceInfo()
+        {
+            return View();
+        }
+
+        public IActionResult teacherAcceptAbsence()
+        {
+            return View();
+        }
+
+        public IActionResult teacherRejectAbsence()
+        {
+            return View();
+        }
+    }
+}
---
+++ KindergartenManagementSystem/Models/Absence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KindergartenManagementSystem.Models
+{
+    // 请假实体类
+    public class Absence
+    {
+        int id { get; set; }
+        int starter { get; set; }
+        int approver { get; set; }
+        DateTime fromTime { get; set; }
+        DateTime toTime { get; set; }
+        string location { get; set; }
+        string reason { get; set; }
+        string rejectReason { get; set; }
+        int status { get; set; }
+    }
+}
